52838820670b23687c4bb24beb91181fcfb33df2
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-08 12:23:20 +0600

    Update with add Comments

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 class Loading extends Component {
     render() {
         return (
+
+            // Spinner Lodaing on the screen untill get data from server
             <div className='d-flex align-items-center justify-content-between my-3'>
                 <h6>Please wait...</h6>
                 <div className="spinner-border text-success " role='status'>
