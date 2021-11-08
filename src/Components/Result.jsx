77e96766db07c2565de5061d62e7e0325cca84af
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-08 12:44:18 +0600

    Add PropTypes & Comments

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Result = props => {
+    //Get props from Parent
     const { totalResults, currentPage, totalPages } = props
     return (
         <div className='d-flex align-items-center justify-content-between my-3'>
+            {/* Show value from server  */}
             <p className='text-black-50'>About {totalResults} result found</p>
             <p className='text-black-50'>{currentPage} of {totalPages} page</p>
         </div>
     )
+}
+
+//PropTypes
+Result.propTypes = {
+    totalResults: PropTypes.number.isRequired,
+    currentPage: PropTypes.number.isRequired,
+    totalPages: PropTypes.number.isRequired
 }
 
 export default Result
