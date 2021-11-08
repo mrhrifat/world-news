deb52f0e7618387c32046c49f429df78726a87f5
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-09 00:57:05 +0600

    Update with PropTypes & Comments

--- src/Components/Paggination.jsx
+++ src/Components/Paggination.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Paggination extends Component {
+
+    //Stop editing the input section until the user double click on paragraph
     state = {
         isEditable: false
     }
     render() {
+        //Get from this component state 
         const { isEditable } = this.state
+
+        //Get from props
         const { isPrev, prev, isNext, next, totalPages, currentPage, handlePageChange, goToPage } = this.props
+
         return (
             <div className='d-flex align-item-center justify-content-between my-4'>
 
-                {/* Previous */}
+                {/* Previous Button */}
                 <button
                     className="btn btn-outline-success btn-sm"
                     disabled={!isPrev}
@@ -21,6 +28,7 @@ class Paggination extends Component {
                     Prev
                 </button>
 
+                {/* Show Page and Input section if double click on paragraph */}
                 <div>
                     {isEditable
                         ?
@@ -29,9 +37,12 @@ class Paggination extends Component {
                             className="form-control form-control-sm"
                             placeholder="Enter page number"
                             value={currentPage}
+                            //Set the current page in Parent News component state > data > currentPage
                             onChange={e => {
                                 handlePageChange(e.target.value)
                             }}
+
+                            //Change the current page with gotoPage() function and make isEditable false
                             onKeyPress={e => {
                                 if (e.key === 'Enter') {
                                     goToPage()
@@ -41,6 +52,8 @@ class Paggination extends Component {
                         />)
                         :
                         (<small
+
+                            //Make isEditable true when double click on paragraph
                             onDoubleClick={() => {
                                 this.setState({
                                     isEditable: !isEditable
@@ -48,13 +61,14 @@ class Paggination extends Component {
                             }
                             }
                         >
+                            {/* Page {currentPage} of {totalPages} */}
                             {currentPage} of {totalPages}
                         </small>
                         )
                     }
                 </div>
 
-                {/* Next */}
+                {/* Next Button */}
                 <button
                     className="btn btn-outline-success btn-sm"
                     disabled={!isNext}
@@ -65,6 +79,18 @@ class Paggination extends Component {
             </div>
         )
     }
+}
+
+//PropTypes
+Paggination.propTypes = {
+    isPrev: PropTypes.bool.isRequired,
+    isNext: PropTypes.bool.isRequired,
+    prev: PropTypes.func.isRequired,
+    next: PropTypes.func.isRequired,
+    totalPages: PropTypes.number.isRequired,
+    currentPage: PropTypes.number.isRequired,
+    handlePageChange: PropTypes.func.isRequired,
+    goToPage: PropTypes.func.isRequired,
 }
 
 export default Paggination
