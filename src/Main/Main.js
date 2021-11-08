7e6bbb4cc510a216f10c13bd8e1bec4069958b55
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-09 01:14:12 +0600

    Update with new functionality and comments

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,5 +1,8 @@
+// Import Axios Instance from AxiosInstance.js
 import axios from './AxiosInstance';
 
+
+// News category based from Server
 export const newsCategory = {
     technology: 'technology',
     general: 'general',
@@ -10,29 +13,84 @@ export const newsCategory = {
     sports: 'sports',
 
 }
-export const country = {
-    ae: 'ae',
+
+// News from country based from server
+export const newsCountry = {
     ar: 'ar',
+    at: 'at',
     au: 'au',
-    cn: 'cn',
-    ua: 'ua',
-    us: 'us',
     be: 'be',
     bg: 'bg',
+    br: 'br',
+    ca: 'ca',
+    ch: 'ch',
+    cn: 'cn',
+    co: 'co',
+    cu: 'cu',
+    cz: 'cz',
+    de: 'de',
+    eg: 'eg',
+    fr: 'fr',
+    gb: 'gb',
+    ae: 'ae',
+    gr: 'gr',
+    hk: 'hk',
+    hu: 'hu',
+    id: 'id',
+    ie: 'ie',
+    il: 'il',
+    in: 'in',
+    it: 'it',
+    jp: 'ip',
+    kr: 'kr',
+    lt: 'li',
+    lv: 'lv',
+    ma: 'ma',
+    mx: 'mx',
+    my: 'my',
+    ng: 'ng',
+    nl: 'nl',
+    no: 'no',
+    nz: 'nz',
+    ph: 'ph',
+    pl: 'pl',
+    pt: 'pt',
+    ro: 'ro',
+    rs: 'rs',
+    ru: 'ru',
+    sa: 'sa',
+    se: 'se',
+    sg: 'sg',
+    si: 'si',
+    sk: 'sk',
+    th: 'th',
+    tr: 'tr',
+    tw: 'tw',
+    ua: 'ua',
+    us: 'us',
+    ve: 've',
+    za: 'za',
 }
 
+// Make most item can visible in one page
 const MAX_ITEM_PER_PAGE = 3
 
+
+// Get news from server by make a class and other functionalities 
 export default class serverNews {
+
+    // Constructor of class
     constructor(country, category) {
-        this._country = country;
-        this._category = category;
+        this._country = country; //This value have to be send from Front End of Main App
+        this._category = category; //This value have to be send from Front End of Main App
         this._search = '';
         this._pageSize = MAX_ITEM_PER_PAGE;
         this._totalPages = 1;
         this._currentPage = 1
     }
 
+
+    // Private function visble for only this class
     _getURL() {
         let url = '/?'
         if (this._country) {
@@ -53,6 +111,7 @@ export default class serverNews {
         return url
     }
 
+    // Send request on server with Async Await Function
     async getNews() {
         // const response = await axios.get(this._getURL())
         // console.log(response.data)
@@ -60,10 +119,11 @@ export default class serverNews {
         try {
             const {
                 data
-            } = await axios.get(this._getURL())
+            } = await axios.get(this._getURL()) //Get news with this._getURL() function
             this._totalPages = Math.ceil(data.totalResults / this._pageSize)
             // console.log(totalPages)
 
+            // Return value to Front End
             return {
                 articles: data.articles,
                 totalResults: data.totalResults,
@@ -82,38 +142,46 @@ export default class serverNews {
 
     }
 
+    // Category Change
     changeCategory(category) {
         this._category = category
         this._currentPage = 1
         return this.getNews()
     }
 
+    // Country Change
     changeCountry(country) {
         this._country = country
         this._currentPage = 1
         return this.getNews()
     }
 
+    // Search Query on Server
     search(initialText) {
         this._search = initialText
         return this.getNews()
     }
 
+    // Reset all value and go to homepage
     homepage() {
         this._currentPage = 1
         this._category = newsCategory.technology
         this._search = ''
+        this._country = newsCountry.us
         return this.getNews()
     }
 
+    // Check is there prev page
     _isPrev() {
         return this._currentPage > 1
     }
 
+    // Check is there next page
     _isNext() {
         return this._currentPage < this._totalPages
     }
 
+    // Go to next page
     nextPage() {
         if (this._isNext()) {
             this._currentPage++
@@ -122,6 +190,7 @@ export default class serverNews {
         return null
     }
 
+    // Go to prev page
     prevPage() {
         if (this._isPrev) {
             this._currentPage--
@@ -130,6 +199,7 @@ export default class serverNews {
         return null
     }
 
+    // Set current page
     setCurrentPage(page) {
         if (page > 0 && page < this._totalPages) {
             this._currentPage = page
@@ -140,3 +210,58 @@ export default class serverNews {
 
 }
 // console.log(`${process.env.REACT_APP_URL}${process.env.REACT_APP_API_KEY}`)
+
+// ae: 'United Arab Emirates',
+// ar: 'Argentina',
+// at: 'Austria',
+// au: 'Australia',
+// be: 'Belgium',
+// bg: 'Bulgaria',
+// br: 'Brazil',
+// ca: 'Canada',
+// ch: 'Switzerland',
+// cn: 'China',
+// co: 'Colombia',
+// cu: 'Cuba',
+// cz: 'Czech Republic',
+// de: 'Germany',
+// eg: 'Egypt',
+// fr: 'France',
+// gb: 'United Kingdom',
+// gr: 'Greece',
+// hk: 'Hong Kong',
+// hu: 'Hungary',
+// id: 'Indonesia',
+// ie: 'Ireland',
+// il: 'Israel',
+// in: 'India',
+// it: 'Italy',
+// jp: 'Japan',
+// kr: 'Korea',
+// lt: 'Lithuania',
+// lv: 'Latvia',
+// ma: 'Morocco',
+// mx: 'Mexico',
+// my: 'Malaysia',
+// ng: 'Nigeria',
+// nl: 'Netherlands',
+// no: 'Norway',
+// nz: 'New Zealand',
+// ph: 'Philippines',
+// pl: 'Poland',
+// pt: 'Portugal',
+// ro: 'Romania',
+// rs: 'Serbia',
+// ru: 'Russia',
+// sa: 'Saudi Arabia',
+// se: 'Sweden',
+// sg: 'Singapore',
+// si: 'Slovenia',
+// sk: 'Slovakia',
+// th: 'Thailand',
+// tr: 'Turkey',
+// tw: 'Taiwan',
+// ua: 'Ukraine',
+// us: 'United States',
+// ve: 'Venezuela',
+// za: 'South Africa',
